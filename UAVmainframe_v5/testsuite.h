b9842b9c8f637352224ddc186e154f5d57f44981
Kai <kai@uav-flightresearch.com>
2017-08-13 20:30:41 +1000

    add card link message
    Signed-off-by: Kai <kai@uav-flightresearch.com>

--- UAVmainframe_v5/testsuite.h
+++ UAVmainframe_v5/testsuite.h
@@ -1140,6 +1140,50 @@ static void mavlink_test_mr_debug(uint8_t system_id, uint8_t component_id, mavli
         MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
 }
 
+static void mavlink_test_thrust_sensor(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
+{
+	mavlink_message_t msg;
+        uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
+        uint16_t i;
+	mavlink_thrust_sensor_t packet_in = {
+		963497464,963497672
+    };
+	mavlink_thrust_sensor_t packet1, packet2;
+        memset(&packet1, 0, sizeof(packet1));
+        	packet1.time_boot_ms = packet_in.time_boot_ms;
+        	packet1.Load_cell = packet_in.Load_cell;
+        
+        
+
+        memset(&packet2, 0, sizeof(packet2));
+	mavlink_msg_thrust_sensor_encode(system_id, component_id, &msg, &packet1);
+	mavlink_msg_thrust_sensor_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+	mavlink_msg_thrust_sensor_pack(system_id, component_id, &msg , packet1.time_boot_ms , packet1.Load_cell );
+	mavlink_msg_thrust_sensor_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+	mavlink_msg_thrust_sensor_pack_chan(system_id, component_id, MAVLINK_COMM_0, &msg , packet1.time_boot_ms , packet1.Load_cell );
+	mavlink_msg_thrust_sensor_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+        mavlink_msg_to_send_buffer(buffer, &msg);
+        for (i=0; i<mavlink_msg_get_send_buffer_length(&msg); i++) {
+        	comm_send_ch(MAVLINK_COMM_0, buffer[i]);
+        }
+	mavlink_msg_thrust_sensor_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+        
+        memset(&packet2, 0, sizeof(packet2));
+	mavlink_msg_thrust_sensor_send(MAVLINK_COMM_1 , packet1.time_boot_ms , packet1.Load_cell );
+	mavlink_msg_thrust_sensor_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+}
+
 static void mavlink_test_UAVmainframe_v5(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
 {
 	mavlink_test_euler_debug(system_id, component_id, last_msg);
@@ -1164,6 +1208,7 @@ static void mavlink_test_UAVmainframe_v5(uint8_t system_id, uint8_t component_id
 	mavlink_test_ekf_debug(system_id, component_id, last_msg);
 	mavlink_test_tecs_debug(system_id, component_id, last_msg);
 	mavlink_test_mr_debug(system_id, component_id, last_msg);
+	mavlink_test_thrust_sensor(system_id, component_id, last_msg);
 }
 
 #ifdef __cplusplus
